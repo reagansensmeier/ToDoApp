cf72fbfe658f5b1dc49bdd86ee77799a468e8fb5
reagan <reaganwiley@gmail.com>
2024-07-03 14:37:36 -0700

    feat: more project folder setup, prisma schema, tailwind set up

---
+++ api/todoActions.tsx
@@ -0,0 +1,22 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import {prisma} from "../lib/prisma"
+
+
+// Example prisma call
+// export const getStaticProps: GetStaticProps = async () => {
+//   const feed = await prisma.post.findMany({
+//     where: { published: true },
+//     include: {
+//       author: {
+//         select: { name: true },
+//       },
+//     },
+//   });
+//   return {
+//     props: { feed },
+//     revalidate: 10,
+//   };
+// };v
+
