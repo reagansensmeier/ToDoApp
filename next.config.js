7ca44021a647de561fea1fdb5c174e41a0b3da82
reagan <reaganwiley@gmail.com>
2024-07-08 09:54:52 -0700

    fix: updating next config to resolve build error

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+
+const path = require("path");
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
